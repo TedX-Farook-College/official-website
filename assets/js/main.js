81b85fa3c04ecb9d1e03a313a12fd809f9f318c6
Shiyas-N <muhammedshiyasn811@gmail.com>
2023-01-07 12:28:59 +0530

    hero section added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 // Navigation Menu
+
+
+
+// tab section
+
+
+function setupTab(){
+    document.querySelectorAll(".tab__button").forEach(button =>{
+        button.addEventListener("click",()=>{
+            const sideBar = button.parentElement;
+            const tabContainer = sideBar.parentElement;
+            const tabNumber = button.dataset.forTab;
+            const tabToActivate = tabContainer.querySelector(`.tab__content[data-tab="${tabNumber}"]`);
+        
+            sideBar.querySelectorAll(".tab__button").forEach(button =>{
+                button.classList.remove("tab__button--active")
+            });
+            tabContainer.querySelectorAll(".tab__content").forEach(tab =>{
+                tab.classList.remove("tab__content--active")
+            });
+
+            button.classList.add("tab__button--active");
+            tabToActivate.classList.add("tab__content--active");
+        
+        });
+    });
+}
+document.addEventListener("DOMContentLoaded",()=>{
+    setupTab();
+
+    //document.querySelectorAll(".tabs").forEach(tabContainer=>{
+    //    tabContainer.querySelector(".tab__sidebar .tab__button"),click();
+    //});
+
+});

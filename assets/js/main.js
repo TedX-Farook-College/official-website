624362e31d1563d1da07dd164ef595b3442a3157
adhilameenet <adhilameenet@gmail.com>
2023-01-08 11:31:53 +0530

    feat:mobile responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,45 @@
 // Navigation Menu
 
+// Dev
 
-// Navbar 
 
-window.addEventListener("scroll", () => {
-    var nav = document.querySelector('.nav')
-     if(window.pageYOffset > 80) {
-        nav.classList.add('nav-bg')
-     } else {
-       nav.classList.remove('nav-bg')
-     }
- })
+// Dev
+
+// Navbar
+
+window.addEventListener('scroll', () => {
+  var nav = document.querySelector('.nav')
+  if (window.pageYOffset > 80) {
+    nav.classList.add('nav-bg')
+  } else {
+    nav.classList.remove('nav-bg')
+  }
+})
 
 // tab section
 
-function setupTab(){
-    document.querySelectorAll(".tab__button").forEach(button =>{
-        button.addEventListener("click",()=>{
-            const sideBar = button.parentElement;
-            const tabContainer = sideBar.parentElement;
-            const tabNumber = button.dataset.forTab;
-            const tabToActivate = tabContainer.querySelector(`.tab__content[data-tab="${tabNumber}"]`);
-        
-            sideBar.querySelectorAll(".tab__button").forEach(button =>{
-                button.classList.remove("tab__button--active")
-            });
-            tabContainer.querySelectorAll(".tab__content").forEach(tab =>{
-                tab.classList.remove("tab__content--active")
-            });
+function setupTab() {
+  document.querySelectorAll('.tab__button').forEach((button) => {
+    button.addEventListener('click', () => {
+      const sideBar = button.parentElement
+      const tabContainer = sideBar.parentElement
+      const tabNumber = button.dataset.forTab
+      const tabToActivate = tabContainer.querySelector(
+        `.tab__content[data-tab="${tabNumber}"]`,
+      )
 
-            button.classList.add("tab__button--active");
-            tabToActivate.classList.add("tab__content--active");
-        
-        });
-    });
+      sideBar.querySelectorAll('.tab__button').forEach((button) => {
+        button.classList.remove('tab__button--active')
+      })
+      tabContainer.querySelectorAll('.tab__content').forEach((tab) => {
+        tab.classList.remove('tab__content--active')
+      })
+
+      button.classList.add('tab__button--active')
+      tabToActivate.classList.add('tab__content--active')
+    })
+  })
 }
-document.addEventListener("DOMContentLoaded",()=>{
-    setupTab();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  setupTab()
+})
